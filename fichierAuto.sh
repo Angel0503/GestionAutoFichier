05e1068eba79f01d73fe43544e1af7b05a6a7bc7
Angel0503 <angel.ag64100@gmail.com>
2022-03-08 13:39:38 +0100

    Debut fichier auto

---
+++ fichierAuto.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+chemin=$(pwd)
+
+#cp $chemin/CompteRendu-OrdreJour/AAAA_MM_JJ_CR.docx $chemin/CompteRendu-OrdreJour/AAAA_MM_JJ_ODJ.docx $chemin/Reunions
+
+jour=$1
+mois=$2
+annee=$3
+
+conversionJour() {
+
+	if [ $jour -lt 10 ]
+	then
+		jour="0"$jour
+	fi
+	echo $jour
+}
+#conversionJour
+
+conversionMois() {
+
+	case $mois in
+	"janvier") mois="01"
+	;;
+	"fevrier") mois="02"
+	;;
+	"mars") mois="03"
+	;;
+	"avril") mois="04"
+	;;
+	"mai") mois="05"
+	;;
+	"juin") mois="06"
+	;;
+	"juillet") mois="07"
+	;;
+	"aout") mois="08"
+	;;
+	"septembre") mois="09"
+	;;
+	"octobre") mois="10"
+	;;
+	"novembre") mois="11"
+	;;
+	"decembre") mois="12"
+	;;
+	esac
+	echo $mois
+}
+#conversionMois
+
+newName() {
+
+	jour=$(conversionJour)
+	mois=$(conversionMois)
+	nomFic=$annee"_"$mois"_"$jour"_""test"
+	echo $nomFic
+}
+#newName
+
+nomFic=$(newName)
+if [ ! -d "Reunions/$nomFic" ]; then
+	mkdir Reunions/$nomFic
+else
+	echo "Fichier deja existant"
+fi
+
+cp $chemin/CompteRendu-OrdreJour/AAAA_MM_JJ_CR.docx $chemin/CompteRendu-OrdreJour/AAAA_MM_JJ_ODJ.docx $chemin/Reunions/$nomFic
+
+mv $chemin/Reunions/$nomFic/AAAA_MM_JJ_CR.docx $chemin/Reunions/$nomFic/$nomFic"_CR".docx
+mv $chemin/Reunions/$nomFic/AAAA_MM_JJ_ODJ.docx $chemin/Reunions/$nomFic/$nomFic"_ODJ".docx
+
